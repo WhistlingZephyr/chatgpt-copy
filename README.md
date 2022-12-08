ccd6af441e12408afe8d1a54efd60e5aeea1e2c4
Zephyr <90854121+WhistlingZephyr@users.noreply.github.com>
2022-12-08 18:51:35 +0530

    feat: first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# ChatGPT Copy
+A UserScript to easily copy ChatGPT conversations to clipboard.
+
+## Instructions
+### Step 1.: Script Manager
+Install one of the following:
+    - https://addons.mozilla.org/firefox/addon/greasemonkey/
+    - https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
+    - https://addons.opera.com/extensions/details/tampermonkey-beta/
+    - https://addons.opera.com/extensions/details/violent-monkey/
+    - https://safari.tampermonkey.net/tampermonkey.safariextz
+    - https://www.microsoft.com/store/p/tampermonkey/9nblggh5162s
+### Step 2. UserScript:
+Install the UserScript from here: https://raw.githubusercontent.com/WhistlingZephyr/chatgpt-copymaster/chatgpt-copy.user.js
+### Step 3. Usage:
+1. Open up https://chat.openai.com/chat.
+2. Refresh if you just logged in.
+3. Open your Script Manager's popout by clicking its icon.
+4. Press "c" or click on the copy option of ChatGPT Copy.
+Optionally, you can activate it from the context menu through right-clicking on the page.
+### Step 4. Config
+After running the script once, you can upon its storage up in your Script Manager's dashboard and change its config.
---
+++ chatgpt-copy.user.js
@@ -0,0 +1,154 @@
+// ==UserScript==
+// @name         ChatGPT Copy
+// @namespace    https://github.com/WhistlingZephyr/chatgpt-copy
+// @homepage     https://github.com/WhistlingZephyr/chatgpt-copy
+// @supportURL   https://github.com/WhistlingZephyr/chatgpt-copy/issues
+// @updateURL    https://raw.githubusercontent.com/WhistlingZephyr/chatgpt-copy/master/chatgpt-copy.user.js
+// @downloadURL  https://raw.githubusercontent.com/WhistlingZephyr/chatgpt-copy/master/chatgpt-copy.user.js
+// @version      0.1
+// @description  A simple UserScript to copy ChatGPT conversations to clipboard
+// @author       WhistlingZephyr
+// @match        https://chat.openai.com/chat
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @grant        GM_getValue
+// @grant        GM_listValues
+// @grant        GM_setValue
+// @grant        GM_notification
+// @grant        GM_setClipboard
+// @grant        GM_registerMenuCommand
+// @run-at       document-idle
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const init = async () => {
+        const values = await GM.listValues();
+        const defaults = {
+            conversationPrefix: '---',
+            userPrefix: 'User: ',
+            botPrefix: 'ChatGPT: ',
+            messageSeperator: '\n',
+            conversationSuffix: '---',
+            conversation: '[]',
+        };
+        for (const key in defaults) {
+            if (Object.hasOwnProperty.call(defaults, key)) {
+                if (!values.includes(key) || !(await GM.getValue(key))) {
+                    await GM.setValue(key, defaults[key]);
+                }
+            }
+        }
+    };
+    const getMessages = async () => {
+        await init();
+        const userPrefix = await GM.getValue('userPrefix');
+        const botPrefix = await GM.getValue('botPrefix');
+        const messageSeperator = await GM.getValue('messageSeperator');
+        return [...document.querySelectorAll('.whitespace-pre-wrap')].map(
+            msg => {
+                return msg.firstChild.tagName
+                    ? botPrefix +
+                          [...msg.querySelectorAll('[class^=request-] > *')]
+                              .map(gptMsg => {
+                                  if (gptMsg.tagName === 'PRE') {
+                                      const code = gptMsg.querySelector('code');
+                                      const language =
+                                          [...code.classList.values()]
+                                              .find(className =>
+                                                  className.startsWith(
+                                                      'language-'
+                                                  )
+                                              )
+                                              ?.replace(/language-/, '') ?? '';
+                                      return (
+                                          '\n```' +
+                                          language +
+                                          '\n' +
+                                          code.innerText +
+                                          '```\n'
+                                      );
+                                  }
+                                  let count = 1;
+                                  return (
+                                      gptMsg.innerHTML
+                                          .replace(/<\/?(?:code|p)>/g, '`')
+                                          .replace(
+                                              /<li>(.+?)<\/li>/g,
+                                              (_match, p1) =>
+                                                  `${count++}. ${p1}\n`
+                                          )
+                                          .trim() + '\n'
+                                  );
+                              })
+                              .join('')
+                              .trim() +
+                          messageSeperator
+                    : userPrefix + msg.innerText.trim() + messageSeperator;
+            }
+        );
+    };
+    const saveToStorage = async () => {
+        const conversation = await getMessages();
+        if (conversation.length > 0) {
+            await GM.setValue(
+                'conversation',
+                JSON.stringify(conversation, null, 4)
+            );
+            return true;
+        }
+        return false;
+    };
+    const formatConversation = async conversation => {
+        await init();
+        const conversationPrefix = await GM.getValue('conversationPrefix');
+        const conversationSuffix = await GM.getValue('conversationSuffix');
+        return [
+            conversationPrefix,
+            conversation.join('\n').trim(),
+            conversationSuffix,
+        ].join('\n');
+    };
+    const copyToClipboard = async () => {
+        await init();
+        let conversation = await getMessages();
+        if (conversation.length == 0) {
+            conversation = JSON.parse(await GM.getValue('conversation'));
+        }
+        const text = await formatConversation(conversation);
+        await GM.setClipboard(text);
+        GM.notification(
+            'Copied the last conversation to clipboard',
+            'Copied successfully!'
+        );
+    };
+    const callback = async () => {
+        const saved = await saveToStorage();
+        if (!saved && (await GM.getValue('conversation')) != '[]') {
+            await copyToClipboard();
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(
+        document.querySelector('[class^=react-scroll-to-bottom-]'),
+        {
+            attributes: false,
+            characterData: false,
+            subtree: true,
+            childList: true,
+        }
+    );
+
+    GM.registerMenuCommand(
+        'Copy the current/last conversation to clipboard',
+        async () => {
+            if ((await GM.getValue('conversation')) != '[]') {
+                await copyToClipboard();
+            }
+        },
+        'c'
+    );
+
+    callback();
+})();
